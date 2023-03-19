95fc65c2f1d87e48b1fc555f6f37823a7fb44f82
勇気 <85111805+ele0210@users.noreply.github.com>
2023-03-19 10:06:05 +0000

    deploy to replit

--- tenor.py
+++ tenor.py
@@ -1,35 +1,38 @@
 import requests
 import json
 import random
+import os
+
 
 def get_trending_gif():
-    # set the apikey and limit
-    apikey = os.environ['TENOR_TOKEN']
-    lmt = 40
+  # set the apikey and limit
+  apikey = os.environ['TENOR_TOKEN']
+  lmt = 40
 
-    r = requests.get(
-        "https://g.tenor.com/v1/trending?key=%s&limit=%s" % (apikey, lmt))
+  r = requests.get("https://g.tenor.com/v1/trending?key=%s&limit=%s" %
+                   (apikey, lmt))
 
-    if r.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        gifs = json.loads(r.content)
-        num = random.randint(0, 39)
-        return gifs['results'][num]['media'][0]['gif']['url']
-    else:
-        return 'https://tenor.com/KZO7.gif'
+  if r.status_code == 200:
+    # load the GIFs using the urls for the smaller GIF sizes
+    gifs = json.loads(r.content)
+    num = random.randint(0, 39)
+    return gifs['results'][num]['media'][0]['gif']['url']
+  else:
+    return 'https://tenor.com/KZO7.gif'
+
 
 def get_search_gif(search):
-    # set the apikey and limit
-    apikey = "71WBT6JZA3JP"
-    lmt = 40
+  # set the apikey and limit
+  apikey = "71WBT6JZA3JP"
+  lmt = 40
 
-    r = requests.get(
-        "https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search, apikey, lmt))
+  r = requests.get("https://g.tenor.com/v1/search?q=%s&key=%s&limit=%s" %
+                   (search, apikey, lmt))
 
-    if r.status_code == 200:
-        # load the GIFs using the urls for the smaller GIF sizes
-        gifs = json.loads(r.content)
-        num = random.randint(0, 39)
-        return gifs['results'][num]['media'][0]['gif']['url']
-    else:
-        return 'https://tenor.com/KZO7.gif'
+  if r.status_code == 200:
+    # load the GIFs using the urls for the smaller GIF sizes
+    gifs = json.loads(r.content)
+    num = random.randint(0, 39)
+    return gifs['results'][num]['media'][0]['gif']['url']
+  else:
+    return 'https://tenor.com/KZO7.gif'
--- youtubeapi.py
+++ youtubeapi.py
@@ -1,34 +1,40 @@
 import json
+import os
 from apiclient.discovery import build
 
+
 def search_youtubevideo(keyword):
-    result = 'ニュース'
-    API_KEY = os.environ['YOUTUBE_TOKEN']
-    YOUTUBE_API_SERVICE_NAME = 'youtube'
-    YOUTUBE_API_VERSION = 'v3'
-    SEARCH_TEXT = keyword
+  result = 'ニュース'
+  API_KEY = os.environ['YOUTUBE_TOKEN']
+  YOUTUBE_API_SERVICE_NAME = 'youtube'
+  YOUTUBE_API_VERSION = 'v3'
+  SEARCH_TEXT = keyword
 
-    youtube = build(
-        YOUTUBE_API_SERVICE_NAME,
-        YOUTUBE_API_VERSION,
-        developerKey=API_KEY
-    )
+  youtube = build(YOUTUBE_API_SERVICE_NAME,
+                  YOUTUBE_API_VERSION,
+                  developerKey=API_KEY)
 
-    response = youtube.search().list(q=SEARCH_TEXT, part='snippet', regionCode='JP', maxResults=3).execute()
+  response = youtube.search().list(q=SEARCH_TEXT,
+                                   part='snippet',
+                                   regionCode='JP',
+                                   maxResults=3).execute()
+
+  for item in response.get('items', []):
+    if item['id']['kind'] != 'youtube#video':
+      continue
+    result = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
+    break
+
+  if result == "ニュース":
+    response = youtube.search().list(q="ニュース",
+                                     part='snippet',
+                                     regionCode='JP',
+                                     maxResults=3).execute()
 
     for item in response.get('items', []):
-        if item['id']['kind'] != 'youtube#video':
-            continue
-        result = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
-        break
+      if item['id']['kind'] != 'youtube#video':
+        continue
+      result = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
+      break
 
-    if result == "ニュース":
-        response = youtube.search().list(q="ニュース", part='snippet', regionCode='JP', maxResults=3).execute()
-
-        for item in response.get('items', []):
-            if item['id']['kind'] != 'youtube#video':
-                continue
-            result = 'https://www.youtube.com/watch?v=' + item['id']['videoId']
-            break
-
-    return result
+  return result
